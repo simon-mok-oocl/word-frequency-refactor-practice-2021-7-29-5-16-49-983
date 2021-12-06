6f3d5452beff676cda3896bf18645f34233c4ed2
moksi3 <simon.mok@oocl.com>
2021-12-06 18:45:36 +0800

    [wf][simon] test: add test for error message

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -53,6 +53,14 @@ public class WordFrequencyGameTest {
         validate_Input_words_process_to_expected_word(inputStr, expectResult);
     }
 
+    @Test
+    public void should_return_error_message_given_null_string() throws Exception {
+        //Given
+        String inputStr = null;
+        String expectResult = "Calculate Error";
+        validate_Input_words_process_to_expected_word(inputStr, expectResult);
+    }
+
     private void validate_Input_words_process_to_expected_word(String inputStr, String expectResult) {
         WordFrequencyGame game = new WordFrequencyGame();
         //When
