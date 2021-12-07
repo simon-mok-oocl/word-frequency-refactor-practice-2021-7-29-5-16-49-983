c4b7ac435812cec1fd8795ffd30ea16039bf3516
moksi3 <simon.mok@oocl.com>
2021-12-07 10:33:40 +0800

    [word freq][simon] refactor: refactor format output

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,42 +3,41 @@ import java.util.stream.Collectors;
 
 
 public class WordFrequencyGame {
-    public String getWordFrequency(String inputStr){
+    final String SPACE_REGEX = "\\s+";
+    final String ERROR_MESSAGE = "Calculate Error";
 
-        String spaceRegex = "\\s+";
-
-            try {
-                List<String> words = Arrays.asList(inputStr.split(spaceRegex));
-                List<WordInfo> wordCount = calWordCount(words);
-
-                wordCount.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                wordCount.stream()
-                        .forEach(word -> joiner.add(String.format("%s %d" , word.getValue() , word.getWordCount())));
-
-                return joiner.toString();
-            } catch (Exception e) {
-
-
-                return "Calculate Error";
-            }
+    public String getWordFrequency(String inputStr) {
+        try {
+            List<WordInfo> wordCount = calWordCount(inputStr);
+            return formatOutputString(wordCount);
+        } catch (Exception e) {
+            return ERROR_MESSAGE;
+        }
     }
 
+    private String formatOutputString(List<WordInfo> wordCount) {
+        wordCount.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-    private List<WordInfo> calWordCount(List<String> sentence)
-    {
-        List<String> distinctWord = sentence.stream()
+        StringJoiner joiner = new StringJoiner("\n");
+        wordCount.stream()
+                .forEach(word -> joiner.add(String.format("%s %d", word.getValue(), word.getWordCount())));
+
+        return joiner.toString();
+    }
+
+    private List<WordInfo> calWordCount(String sentence) {
+        List<String> words = Arrays.asList(sentence.split(SPACE_REGEX));
+        List<String> distinctWord = words.stream()
                 .distinct()
                 .collect(Collectors.toList());
 
         List<WordInfo> wordCount = new ArrayList<>();
-        
+
         distinctWord.stream()
-                .forEach( targetWord ->
+                .forEach(targetWord ->
                 {
-                    int count = (int) sentence.stream().filter(word -> word.equals(targetWord)).count();
-                    wordCount.add(new WordInfo(targetWord , count));
+                    int count = (int) words.stream().filter(word -> word.equals(targetWord)).count();
+                    wordCount.add(new WordInfo(targetWord, count));
                 });
 
         return wordCount;
